1d853e04320aa6800178c097a18549a45d4fc1eb
mozrt2 <moritz.boullenger@outlook.com>
2023-07-21 20:28:02 +0200

    safe sdk get owned safes

--- frontend/components/safeEthersAdapter.tsx
+++ frontend/components/safeEthersAdapter.tsx
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers'
 import { EthersAdapter } from '@safe-global/protocol-kit'
+import { EthAdapter } from '@safe-global/safe-core-sdk-types'
 
 const provider = new ethers.providers.JsonRpcProvider("https://rpc.gnosis.gateway.fm")
 const safeOwner = provider.getSigner(0)
@@ -7,4 +8,6 @@ const safeOwner = provider.getSigner(0)
 const ethAdapter = new EthersAdapter({
   ethers,
   signerOrProvider: safeOwner
-})
+}) as unknown as EthAdapter
+
+export default ethAdapter
